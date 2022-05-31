377f59da38cc05b51118d934784470b63e3bdc42
gurcancekic <gurcan.cekic@reengen.com>
2022-05-31 23:04:25 +0300

    Basic Types, Values and Pointers - Part II

--- main.go
+++ main.go
@@ -3,11 +3,86 @@ package main
 import "fmt"
 
 func main() {
+	first := 100
+	var second *int
+
+	fmt.Println(*second)
+
+	fmt.Println(first)
+	fmt.Println(second)
+
+	second = &first
+
+	fmt.Println(first)
+	fmt.Println(second)
+}
+
+/* package main
+
+import "fmt"
+
+func main() {
+	first := 100
+	second := &first
+
+	first++
+	*second++
+
+	fmt.Println(first)
+	fmt.Println(*second)
+
+	var myNewPointer *int = second
+	*myNewPointer++
+
+	fmt.Println(first)
+	fmt.Println(*second)
+	fmt.Println(*myNewPointer)
+
+} */
+
+/* package main
+
+import "fmt"
+
+func main() {
+
+	price, tax, inStock, _ := 275.00, 27.5, true, true
+
+	var _ = "Alice"
+
+	fmt.Println("Total: ", price+tax)
+	fmt.Println("InStock: ", inStock)
+
+	price2, tax := 250.00, 27.5
+	fmt.Println("Total: ", price2+tax)
+} */
+
+/* package main
+
+import "fmt"
+
+func main() {
+
+	 	var price, tax = 275.00, 27.50
+
+	   	fmt.Println(price + tax)
+
+	var price, tax float64
+	price = 275.00
+	tax = 27.50
+	fmt.Println(price + tax)
+} */
+
+/* package main
+
+import "fmt"
+
+func main() {
 	var price float32
 	fmt.Println(price)
 	price = 300
 	fmt.Println(price)
-}
+} */
 
 /* package main
 
