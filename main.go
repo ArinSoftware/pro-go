2488834a5450ff7a105035ca9f81cb36b7fe5559
gurcancekic <gurcan.cekic@reengen.com>
2022-06-02 23:29:24 +0300

    operators & conversions partII

--- main.go
+++ main.go
@@ -1,5 +1,115 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	val1 := "100"
+
+	if int1, int1err := strconv.ParseInt(val1, 2, 8); int1err != nil {
+		fmt.Println("Can not parse:: ", val1)
+	} else {
+		smallInt := int8(int1)
+		fmt.Println("smallInt:: ", smallInt)
+	}
+}
+
+/* package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	val1 := "not false"
+
+	if bool1, b1err := strconv.ParseBool(val1); b1err != nil {
+		fmt.Println("Can not parse: ", val1)
+	} else {
+		fmt.Println("Bool1: ", bool1)
+	}
+
+} */
+
+/* package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	val1 := "false"
+
+	bool1, b1err := strconv.ParseBool(val1)
+
+	if b1err != nil {
+		fmt.Println("can not parse: ", val1)
+	} else {
+		fmt.Println("Bool1: ", bool1)
+	}
+} */
+
+/* package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	val1 := "true"
+	val2 := "false"
+	val3 := "not true"
+
+	bool1, b1err := strconv.ParseBool(val1)
+	bool2, b2err := strconv.ParseBool(val2)
+	bool3, b3err := strconv.ParseBool(val3)
+
+	fmt.Println("Bool1", bool1, b1err)
+	fmt.Println("Bool2", bool2, b2err)
+	fmt.Println("Bool3", bool3, b3err)
+
+} */
+
+/* package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	value := 2.7
+
+	fmt.Println(math.Round(value))
+	fmt.Println(math.Ceil(value))
+	fmt.Println(math.Floor(value))
+	fmt.Println(math.RoundToEven(value))
+} */
+
+/* package main
+
+import "fmt"
+
+func main() {
+	num1 := 275.50
+	num2 := 25
+
+	fmt.Println(int(num1) + num2)
+
+	num3 := 275.50
+	num4 := 25
+
+	fmt.Println(num3 + float64(num4))
+
+} */
+
+/* package main
+
 import "fmt"
 
 func main() {
@@ -16,7 +126,7 @@ func main() {
 	fmt.Println(*second == *third)
 	fmt.Println(*second == *beta)
 
-}
+} */
 
 /* package main
 
