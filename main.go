6df688a4c66c99a8a9bd62c6c5d58d7e8f99160a
gurcancekic <gurcan.cekic@reengen.com>
2022-06-04 11:49:12 +0300

    flow control part I

--- main.go
+++ main.go
@@ -6,159 +6,48 @@ import (
 )
 
 func main() {
-	val1 := "100"
+	priceString := "100"
 
-	if int1, int1err := strconv.ParseInt(val1, 2, 8); int1err != nil {
-		fmt.Println("Can not parse:: ", val1)
+	if kayakPrice, err := strconv.Atoi(priceString); err != nil {
+		fmt.Println("Cannot Parse", priceString)
 	} else {
-		smallInt := int8(int1)
-		fmt.Println("smallInt:: ", smallInt)
+
+		fmt.Println("Parsed value", kayakPrice)
 	}
 }
 
 /* package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	val1 := "not false"
+	price := 250
 
-	if bool1, b1err := strconv.ParseBool(val1); b1err != nil {
-		fmt.Println("Can not parse: ", val1)
+	if price > 500 {
+		scopedVar := 500
+		fmt.Println("Price is higher than ", scopedVar)
+	} else if price <= 200 {
+		scopedVar := 200
+		fmt.Println("Price is lower than or equal ", scopedVar)
 	} else {
-		fmt.Println("Bool1: ", bool1)
-	}
-
-} */
-
-/* package main
-
-import (
-	"fmt"
-	"strconv"
-)
-
-func main() {
-	val1 := "false"
-
-	bool1, b1err := strconv.ParseBool(val1)
-
-	if b1err != nil {
-		fmt.Println("can not parse: ", val1)
-	} else {
-		fmt.Println("Bool1: ", bool1)
+		scopedVar := false
+		fmt.Println("Match", scopedVar)
 	}
 } */
 
 /* package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
-func main() {
-	val1 := "true"
-	val2 := "false"
-	val3 := "not true"
-
-	bool1, b1err := strconv.ParseBool(val1)
-	bool2, b2err := strconv.ParseBool(val2)
-	bool3, b3err := strconv.ParseBool(val3)
-
-	fmt.Println("Bool1", bool1, b1err)
-	fmt.Println("Bool2", bool2, b2err)
-	fmt.Println("Bool3", bool3, b3err)
-
-} */
-
-/* package main
-
-import (
-	"fmt"
-	"math"
-)
-
-func main() {
-	value := 2.7
-
-	fmt.Println(math.Round(value))
-	fmt.Println(math.Ceil(value))
-	fmt.Println(math.Floor(value))
-	fmt.Println(math.RoundToEven(value))
-} */
-
-/* package main
-
 import "fmt"
 
 func main() {
-	num1 := 275.50
-	num2 := 25
+	price := 250
 
-	fmt.Println(int(num1) + num2)
+	if price > 500 {
+		fmt.Println("Price is higher than 500")
+	} else if price <= 200 {
+		fmt.Println("Price is lower than or equal 200")
+	} else {
+		fmt.Println("Price is did not match")
+	}
 
-	num3 := 275.50
-	num4 := 25
-
-	fmt.Println(num3 + float64(num4))
-
-} */
-
-/* package main
-
-import "fmt"
-
-func main() {
-	first := 100
-	second := &first
-	third := &first
-
-	alfa := 100
-	beta := &alfa
-
-	fmt.Println(second == third)
-	fmt.Println(second == beta)
-	fmt.Println()
-	fmt.Println(*second == *third)
-	fmt.Println(*second == *beta)
-
-} */
-
-/* package main
-
-import (
-	"fmt"
-	"math"
-)
-
-func main() {
-	posResult := 3 % 2
-	negResult := -3 % 2
-	absResult := math.Abs(float64(negResult))
-
-	fmt.Println(posResult)
-	fmt.Println(negResult)
-	fmt.Println(absResult)
-} */
-
-/* package main
-
-import "fmt"
-
-func main() {
-	price, tax := 275.00, 27.40
-
-	sum := price + tax
-	difference := price - tax
-	product := price * tax
-	quotient := price / tax
-
-	fmt.Println(sum)
-	fmt.Println(difference)
-	fmt.Println(product)
-	fmt.Println(quotient)
 } */
